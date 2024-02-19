f97b9db6586bd9afbd2c0f4f028ca3afd0e0b94e
diegoperea20 <diegoperea20@gmail.com>
2024-02-19 11:48:33 -0500

    update information specialization

--- src/Education.jsx
+++ src/Education.jsx
@@ -7,8 +7,7 @@ function Education() {
         <a className='my-education-u' href='https://www.uao.edu.co/'>Universidad Autonoma de Occidente , Cali - Colombia</a>
         
               <ul>
-        <li><a className='my-education-list'> Mechatronics engineer and artificial intelligence
-        specialization in process
+        <li><a className='my-education-list'> Mechatronics engineer and and specialization in artificial intelligence 
         </a></li><br/>
         <li><a className='my-education-list' href='https://drive.google.com/file/d/1M7iQ-3HrFvVidRWsKGdACL1Nj9MgpT3Q/view'>Partipation in the 7th Meeting of initiation of Student Research and Innovation in Engineering INVESTIGA & INNOVA 2022</a></li><br/>
         <li><a className='my-education-list'>Participation of reading and writing seedbed
--- src/Myself.jsx
+++ src/Myself.jsx
@@ -28,7 +28,7 @@ function Myself() {
             <div className='my-descriptioni'>
             <p>
             Curious Mechatronic engineer  with specialization in
-            artificial intelligence (in process) who loves to learn, adapts and
+            artificial intelligence who loves to learn, adapts and
             grows in his knowledge, effective leader who knows how to identify
             when it is time to lead. Prioritizes growth over stability.
             Innovative thinking. Able to work in a team and self-manage.

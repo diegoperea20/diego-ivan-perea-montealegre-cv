fe4f4d6423ce94e753f724fcafcd7018423a1d0d
diegoperea20 <diegoperea20@gmail.com>
2024-08-02 16:25:09 -0500

    improved titles experience

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -7,9 +7,9 @@ function Experience() {
     <div className='my-projects'>
         <h2 className='my-experince-subtiitle '  id="Experience">Work Experience</h2>
         
-        <CardProject url="https://github.com/JeickH" title="Software development in EUPHORIA " text='Full Stack python in online store development and deployment with methodology 
+        <CardProject url="https://github.com/JeickH" title="Software Engineer in EUPHORIA " text='Full Stack python in online store development and deployment with methodology 
         agile. 2023 '/>
-        <CardProject url="https://www.pressex.co/" title="Full-stack developer in Pressex Logistics " text="Backend Python and frontend React software development of ERP (enterprise resource planning). 2024 
+        <CardProject url="https://www.pressex.co/" title="Full-stack Developer in Pressex Logistics " text="Backend Python and frontend React software development of ERP (enterprise resource planning). 2024 
           "/>
        
         

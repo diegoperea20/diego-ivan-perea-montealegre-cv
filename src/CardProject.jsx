1ee085735d2b2e06c2e754de3ebb6d312d4bb842
diegoperea20 <diegoperea20@gmail.com>
2024-08-02 18:38:34 -0500

    added prop items

--- src/CardProject.jsx
+++ src/CardProject.jsx
@@ -10,6 +10,13 @@ function CardProject(props) {
         <div className='my-card-text-div'>
         <h2 className='my-card-text'>{props.text}</h2>
         </div>
+        {props.items && (
+            <ul className='my-card-items'>
+              {props.items.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          )}
     </div>
   )
 }
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -8,7 +8,7 @@ function Experience() {
         <h2 className='my-experince-subtiitle '  id="Experience">Work Experience</h2>
         
         <CardProject url="https://github.com/JeickH" title="Software Engineer in EUPHORIA " text='Full Stack python in online store development and deployment with methodology 
-        agile. 2023 '/>
+        agile. 2023 ' items={["Backend: Database administration, inventory management and shopping cart, using Wompi API (payment gateway), sending html emails and  deployment on AWS.", "Fronted: Creation of header, image carousel, pop-ups, footer, categorical filters and responsive design."]}/>
         <CardProject url="https://www.pressex.co/" title="Full-stack Developer in Pressex Logistics " text="Backend Python and frontend React software development of ERP (enterprise resource planning). 2024 
           "/>
        

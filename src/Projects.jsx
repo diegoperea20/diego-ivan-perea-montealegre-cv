c9ec0c592c33d45271fe80d049aaf2ae39b10144
diego ivan perea montealegre <107358939+diegoperea20@users.noreply.github.com>
2025-09-04 21:32:46 -0500

    feat: project open video meet

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -6,6 +6,7 @@ function Projects() {
   return (
     <div className='my-projects'>
         <h2 className='my-project-subtiitle'  id="Projects">Projects <h6>More than 160 projects in github</h6></h2>
+        <CardProject url="https://github.com/diegoperea20/video-meet-rooms" title="Open Video Meet" text="Open Video Meet is a modern web videoconferencing application built with Next.js and Socket It allows users to create meeting rooms, join them, and enjoy high-quality video calls with advanced features like screen sharing and real-time chat. 2025"/>
         <CardProject url="https://subtrackerd.vercel.app/" title="SubTracker" text="Take control of your recurring payments. Monitor, analyze, and optimize your subscription spending with our powerful tracking platform. 2025 "/>
         <CardProject url="https://presentation-recording.vercel.app/" title="Presentation-recording" text="A modern web application for recording and managing presentations. 2025 "/>
         <CardProject url="https://urlshortens.vercel.app/" title="Url Shortener" text="A modern, fast, and user-friendly URL shortener. 2025 "/>
